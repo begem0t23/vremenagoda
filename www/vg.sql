df75ad31ee2668ef08a6092a78085a6983c07a30
begem0t23 <aabogachev@gmail.com>
2014-08-02 23:07:14 +0400

    бд

--- www/vg.sql
+++ www/vg.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Июл 31 2014 г., 16:11
+-- Время создания: Авг 02 2014 г., 23:06
 -- Версия сервера: 5.5.25
 -- Версия PHP: 5.3.13
 
@@ -23,6 +23,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `clients`
+--
+
+CREATE TABLE IF NOT EXISTS `clients` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` tinytext NOT NULL,
+  `phone` tinytext NOT NULL,
+  `email` tinytext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `orders`
+--
+
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `creatorid` int(10) unsigned NOT NULL,
+  `createdate` datetime NOT NULL,
+  `clientid` int(10) unsigned NOT NULL,
+  `eventdate` datetime NOT NULL,
+  `status` tinyint(1) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `users`
 --
 
@@ -31,7 +61,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `login` tinytext NOT NULL,
   `pass` tinytext NOT NULL,
   `realname` tinytext NOT NULL,
-  `field` int(11) NOT NULL,
+  `role` tinyint(1) unsigned NOT NULL DEFAULT '1',
   `isactive` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=2 ;
@@ -40,8 +70,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`id`, `login`, `pass`, `realname`, `field`, `isactive`) VALUES
-(1, 'aabogachev@gmail.com', '6c14da109e294d1e8155be8aa4b1ce8e', 'Bogachev', 0, 1);
+INSERT INTO `users` (`id`, `login`, `pass`, `realname`, `role`, `isactive`) VALUES
+(1, 'aabogachev@gmail.com', '6c14da109e294d1e8155be8aa4b1ce8e', 'Bogachev', 5, 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
